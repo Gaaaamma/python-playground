8b03cebbe4d6a8ee85fecedef6f920e99f0b9dce
Gaaaamma <ytching552022@gmail.com>
2024-07-25 12:28:29 +0800

    Chore: switch create_account and adduser logic

--- adduser.sh
+++ adduser.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-usage() {
-    echo "Usage: ./adduser.sh {container_name}"
-    exit 1
+# Function to add a user if they do not already exist
+add_user() {
+    local username=$1
+    local password=$2
+
+    # Check if the user already exists
+    if id "$username" &>/dev/null; then
+        echo "User $username already exists. Password will not be changed."
+    else
+        # Create the user with a home directory and append to student group
+        sudo useradd -m -s /bin/bash -G student "$username"
+        if [ $? -eq 0 ]; then
+            echo "$username:$password" | sudo chpasswd
+            echo "User $username has been created with the default password and added to student group."
+        else
+            echo "Failed to create user $username."
+        fi
+    fi
 }
 
-# Check if the correct number of arguments is provided
-if [ $# -ne 1 ]; then
-    usage
+# Ensure the student group exists
+if ! getent group student >/dev/null; then
+    sudo groupadd student
 fi
 
-CONTAINER_NAME=$1
-ACCOUNT_SCRIPT=create_account.sh
-
-docker cp $ACCOUNT_SCRIPT sandbox:/bin/$ACCOUNT_SCRIPT
-docker exec -it $CONTAINER_NAME /bin/bash -c $ACCOUNT_SCRIPT
+# Add users here
+add_user "demo" "demo"
--- create_account.sh
+++ create_account.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-# Function to add a user if they do not already exist
-add_user() {
-    local username=$1
-    local password=$2
-
-    # Check if the user already exists
-    if id "$username" &>/dev/null; then
-        echo "User $username already exists. Password will not be changed."
-    else
-        # Create the user with a home directory and append to student group
-        sudo useradd -m -s /bin/bash -G student "$username"
-        if [ $? -eq 0 ]; then
-            echo "$username:$password" | sudo chpasswd
-            echo "User $username has been created with the default password and added to student group."
-        else
-            echo "Failed to create user $username."
-        fi
-    fi
+usage() {
+    echo "Usage: ./create_account.sh {container_name}"
+    exit 1
 }
 
-# Ensure the student group exists
-if ! getent group student >/dev/null; then
-    sudo groupadd student
+# Check if the correct number of arguments is provided
+if [ $# -ne 1 ]; then
+    usage
 fi
 
-# Add users here
-add_user "demo" "demo"
+CONTAINER_NAME=$1
+ADD_USER_SCRIPT=adduser.sh
+
+docker cp $ADD_USER_SCRIPT sandbox:/bin/$ADD_USER_SCRIPT
+docker exec -it $CONTAINER_NAME /bin/bash -c $ADD_USER_SCRIPT
